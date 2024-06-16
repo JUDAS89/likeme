bd9c8969ffa70b3d84092721c34dbcfeb1aa20f9
JUDAS89 <lorena.peroza@outlook.com>
2024-06-15 23:55:45 -0400

    corregir importacion

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import pkg from 'pg'
 const { Pool } = pkg
-import posts from './data.js' // Importar el array temporal
 import dotenv from 'dotenv' // Variables de entorno
 
 // Cargar las variables de entorno desde el archivo .env
